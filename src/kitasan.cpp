87a90790e3436bec586af7aecdfbd98360d28012
Assem Yeleussiz <asemeleusiz0022@gmail.com>
2026-05-12 21:29:08 +0100

    palindrome number leetcode

--- src/kitasan.cpp
+++ src/kitasan.cpp
@@ -1,9 +1,46 @@
+#include <string>
 #include <iostream>
+#include <vector>
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        std::vector<bool> isPalindromeVector;
 
-int add(int x, int y);
+        std::string stringX = std::to_string(x);
+        std::cout << "stringX: " << stringX << '\n';
+
+        int leftCharIndex = 0;
+        int middleIndex = stringX.length() / 2;
+        while (leftCharIndex < middleIndex)
+        {
+            char leftChar = stringX.at(leftCharIndex);
+            int rightCharIndex = stringX.length() - leftCharIndex - 1;
+            char rightChar = stringX.at(rightCharIndex);
+            std::cout << "left: " << leftChar << " | right: " << rightChar;
+
+            bool areDifferent = leftChar != rightChar;
+            std::cout << " | different?: " << areDifferent << '\n';
+
+            if (areDifferent)
+            {
+                return false;
+            }
+            leftCharIndex++;
+        }
+
+        return true;
+    }
+};
 
 int main()
 {
-    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n'; // compile error
+    std::cout << std::boolalpha;
+    Solution s;
+    int testInt = -121;
+    bool isP = s.isPalindrome(testInt);
+    std::cout << "is palindrome: " << isP << '\n';
+
     return 0;
 }
