d72c451df7dcdfb52cb271bbd09188c5b4c4128d
Assem Yeleussiz <asemeleusiz0022@gmail.com>
2026-05-15 18:23:02 +0100

    including header files

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # learncpp meow
 
-## 0.13
+## (0.13) Changing c++ version
+
+added this to tasks.json args
+
+```json
+"-std=c++23",
+```
 
 if run code with in built vscode button it's c++ 23, if use coderunner extension shortcut it used to be c++ 14. so i went to code runner and changed the cpp command to include -std=c++23:
 
@@ -8,9 +14,30 @@ if run code with in built vscode button it's c++ 23, if use coderunner extension
 "cpp": "clang++ -std=c++23 src/*.cpp -o $fileNameWithoutExt && ./$fileNameWithoutExt #",
 ```
 
-## 2.8
+## (2.8) Compiling mutliple files
 
-added this line to compile multiple files
+added this to tasks.json args
+
 ```json
 "${fileDirname}/**.cpp",
+```
+
+## (2.11) Including header files
+
+added this to tasks.json args
+
+```json
+"-I../src/include",
+```
+
+added this to c_cpp_properties.json includePath (should be for intellisense, although works without it too)
+
+```json
+"${workspaceFolder}/src/include"
+```
+
+for code runner extension changed the command to this (-Idirname will make it search for header files in the dirname directory):
+
+```json
+"cpp": "clang++ -Isrc/include -std=c++23 src/*.cpp -o $fileNameWithoutExt && ./$fileNameWithoutExt #",
 ```
--- src/kitasan.cpp
+++ src/kitasan.cpp
@@ -1,50 +1,9 @@
-#include <string>
 #include <iostream>
-#include <vector>
-#include <map>
-class Solution
-{
-public:
-    int romanToInt(std::string s)
-    {
-        std::map<char, int> romanNums = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-
-        int resultInt{};
-        for (size_t i = 0; i < s.length(); i++)
-        {
-            bool isIbeforeVX = (s.at(i) == 'I') && (s.at(i + 1) == 'V' || s.at(i + 1) == 'X');
-            bool isXbeforeLC = (s.at(i) == 'X') && (s.at(i + 1) == 'L' || s.at(i + 1) == 'C');
-            bool isCbeforeDM = (s.at(i) == 'C') && (s.at(i + 1) == 'd' || s.at(i + 1) == 'M');
-            if (isIbeforeVX || isXbeforeLC || isCbeforeDM)
-            {
-                resultInt = romanNums[s.at(i + 1)] - romanNums[s.at(i)];
-                if (i == (s.length() - 1))
-                {
-                    break;
-                }
-                else
-                {
-                    i++;
-                }
-            }
-            else
-            {
-                resultInt += romanNums[s.at(i)];
-            }
-        }
-        return resultInt;
-    }
-};
+#include "add.h"
 
 int main()
 {
-    std::cout << std::boolalpha;
-    Solution s;
-
-    std::string romanString = "LVIII";
-    int result = s.romanToInt(romanString);
-
-    std::cout << "result: " << result << '\n';
-
+    int x = add(3, 4);
+    std::cout << "!!!! meow :3 x: " << x << '\n';
     return 0;
 }
